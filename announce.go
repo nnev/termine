36f994ed1d5a562e17548944c9cfd55d5bf5a3ab
Jonas Große Sundrup <cherti@letopolis.de>
2016-05-29 20:50:49 +0200

    implement suggested changes

--- announce.go
+++ announce.go
@@ -13,8 +13,8 @@ import (
 
 var cmdAnnounce = &Command{
 	UsageLine: "announce",
-	Short:     "Announced nächsten Stammtisch oder c1/4",
-	Long: `Announced den nächsten Stammtisch oder die nächste c1/4,
+	Short:     "Kündigt nächsten Stammtisch oder nächste c¼h an",
+	Long: `Announced den nächsten Stammtisch oder die nächste c¼h,
 je nachdem, was am nächsten Donnerstag ist.`,
 	Flag:         flag.NewFlagSet("announce", flag.ExitOnError),
 	NeedsDB:      true,
@@ -53,26 +53,25 @@ Damit wir passend reservieren können, tragt bitte bis Dienstag Abend,
 	type data struct {
 		Location string
 	}
-	err = mailtmpl.Execute(mailbuf, data{loc})
-	mail := mailbuf.String()
+	if err = mailtmpl.Execute(mailbuf, data{loc}); err != nil {
+		fmt.Fprintln(os.Stderr, "Fehler beim Füllen des Templates:", err)
+		return
+	}
+	mail := mailbuf.Bytes()
 
 	sendAnnouncement("Bitte für Stammtisch eintragen", mail)
-
 }
 
 func announceC14(date time.Time) {
-
-	type data struct {
-		Topic, Abstract, Speaker string
+	var data struct {
+		Topic,
+		Abstract,
+		Speaker string
 	}
 
-	d := new(data)
-
-	err := db.QueryRow("SELECT topic FROM vortraege WHERE date = $1", date).Scan(&d.Topic)
-	if err != nil {
-
+	if err := db.QueryRow("SELECT topic FROM vortraege WHERE date = $1", date).Scan(&data.Topic); err != nil {
 		if err == sql.ErrNoRows {
-			fmt.Println("Es gibt nächsten Donnerstag noch keine c1/4. :(")
+			fmt.Println("Es gibt nächsten Donnerstag noch keine c¼h. :(")
 			return
 		}
 
@@ -80,26 +79,23 @@ func announceC14(date time.Time) {
 		return
 	}
 
-	err = db.QueryRow("SELECT abstract FROM vortraege WHERE date = $1", date).Scan(&d.Abstract)
-	if err != nil {
+	if err := db.QueryRow("SELECT abstract FROM vortraege WHERE date = $1", date).Scan(&data.Abstract); err != nil {
 		fmt.Fprintln(os.Stderr, "Kann abstract nicht auslesen:", err)
 		return
 	}
 
-	err = db.QueryRow("SELECT speaker FROM vortraege WHERE date = $1", date).Scan(&d.Speaker)
-	if err != nil {
+	if err := db.QueryRow("SELECT speaker FROM vortraege WHERE date = $1", date).Scan(&data.Speaker); err != nil {
 		fmt.Fprintln(os.Stderr, "Kann speaker nicht auslesen:", err)
 		return
 	}
 
 	maildraft := `Liebe Treffler,
 
-am kommenden Donnerstag wird es um das Thema
+am kommenden Donnerstag wird {{.Speaker}} eine c¼h zum Thema
 
     {{.Topic}}
 
-gehen. Es spricht {{.Speaker}} zu uns.
-
+halten.
 
 Kommet zahlreich!
 
@@ -111,35 +107,26 @@ Wer mehr Informationen möchte:
 
 	mailtmpl := template.Must(template.New("maildraft").Parse(maildraft))
 	mailbuf := new(bytes.Buffer)
-	err = mailtmpl.Execute(mailbuf, d)
-	mail := mailbuf.String()
-	sendAnnouncement(d.Topic, mail)
+	if err := mailtmpl.Execute(mailbuf, data); err != nil {
+		fmt.Fprintln(os.Stderr, "Fehler beim Füllen des Templates:", err)
+		return
+	}
+	mail := mailbuf.Bytes()
+	sendAnnouncement(data.Topic, mail)
 }
 
-func sendAnnouncement(subject, msg string) error {
-	fromheader := "From: termine@noname-ev.de"
-	toheader := "To: cherti@letopolis.de"
+func sendAnnouncement(subject string, msg []byte) error {
+	fromheader := "From: frank@noname-ev.de"
+	toheader := "To: ccchd@ccchd.de"
 	subjectheader := "Subject: " + subject
-	fullmail := fromheader + "\n" + subjectheader + "\n" + toheader + "\n\n" + msg
+	fullmail := []byte(fromheader + "\n" + subjectheader + "\n" + toheader + "\n\n")
+	fullmail = append(fullmail, msg...)
 
-	fmt.Println(":: creating command")
 	cmd := exec.Command("/usr/sbin/sendmail", "-t")
 
-	fmt.Println(":: setting up stdin")
-	indump, err := cmd.StdinPipe()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Fehler beim Senden: ", err)
-	}
+	cmd.Stdin = bytes.NewReader(fullmail)
 
-	fmt.Println(":: write to stdin")
-	_, err = indump.Write([]byte(fullmail))
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Fehler beim Senden: ", err)
-	}
-	indump.Close()
-
-	err = cmd.Run()
-	if err != nil {
+	if err := cmd.Run(); err != nil {
 		fmt.Fprintln(os.Stderr, "Fehler beim Senden: ", err)
 	}
 
@@ -147,8 +134,7 @@ func sendAnnouncement(subject, msg string) error {
 }
 
 func RunAnnounce() {
-	// get next donnerstag, bool stammtisch is true oder false
-	nextThursday := getNextThursdays(3)[2]
+	nextThursday := getNextThursdays(1)[0]
 
 	isStm, err := isStammtisch(nextThursday)
 	if err != nil {
